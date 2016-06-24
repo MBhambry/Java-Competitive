fe2cb731979c49349f0691976312f498ef02513c
MBhambry <mohaklee@gmail.com>
2016-06-24 16:37:33 +0530

    Java End-of-File

---
+++ JavaEndofFile.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class JavaEndofFile {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+          Scanner sc = new Scanner(System.in);
+          int t = 1;
+          while(sc.hasNext()){
+        	 System.out.println(t + " " + sc.nextLine());
+        	 t++;
+        	  
+          }
+    
+    }
+}
+
+
--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Solution - Solution.java
 Problem # 2 Java Datatypes
 Problem Statement - https://www.hackerrank.com/challenges/java-datatypes/ <br />
 Solution - JavaDatatypes.java
+
+Problem # 3 Jave End-of-File 
+Problem Statement - https://www.hackerrank.com/challenges/java-end-of-file/ <br />
+Solution  - JavaEndofFile.java
